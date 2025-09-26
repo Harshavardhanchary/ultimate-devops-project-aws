8a4789b07579e229a5f41689e95754a145b6d2ce
Harshavardhanchary <harshavardhanchary7@gmail.com>
2025-09-26 08:57:21 +0530

    modified terraform scripts

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "harsha-demo-terraform-eks-state-s3-bucket"
 
   lifecycle {
     prevent_destroy = false
@@ -27,13 +27,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-eks-state-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
