57b920a3c84f15de7a5ebec7d7f45daa9246d8b2
root <root@srv996978.hstgr.cloud>
2025-10-01 05:14:21 +0000

    edited region and instance types

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -53,7 +53,7 @@ variable "node_groups" {
   }))
   default = {
     general = {
-      instance_types = ["t3.small"]
+      instance_types = ["m7i-flex.large"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
         desired_size = 2
